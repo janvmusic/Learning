d472a3522c47b7f8a1b32f1e420e305f2c02937a
janvmusic <djmatt25@gmail.com>
2020-11-08 22:15:57 -0700

    Split implementations from gear and wheel

--- practical-ood/code/gear.rb
+++ practical-ood/code/gear.rb
@@ -1,10 +1,10 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel = nil)
     @chainring = chainring
     @cog       = cog
-    @wheel     = Wheel.new(rim, tire)
+    @wheel     = wheel
   end
 
   def ratio
@@ -15,23 +15,4 @@ class Gear
     # tire goes around rim twice for diameter
     ratio * wheel.diameter
   end
-
-  # Embeding wheel here, adds the idea that should only exists in gear context
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim + (tire * 2)
-    end
-  end
 end
-
-# puts Gear.new(52, 11).ratio
-# => 4.7272727272727275
-
-# puts Gear.new(30, 27).ratio
-# => 1.1111111111111112
-
-# puts Gear.new(52, 11, 26, 1.5).gear_inches
-# => 137.0909090909091
-
-# puts Gear.new(52, 11, 24, 1.25).gear_inches
-# =>125.27272727272728
