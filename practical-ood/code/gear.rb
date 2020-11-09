622586e277be02c2e42e1154ba463d65aef53af2
janvmusic <djmatt25@gmail.com>
2020-11-08 22:11:28 -0700

    Extract wheel from gear

--- practical-ood/code/gear.rb
+++ practical-ood/code/gear.rb
@@ -1,13 +1,26 @@
 class Gear
-  attr_reader :chainring, :cog
+  attr_reader :chainring, :cog, :rim, :tire
 
-  def initialize(chainring, cog)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog       = cog
+    @wheel     = Wheel.new(rim, tire)
   end
 
   def ratio
     chainring / cog.to_f
+  end
+
+  def gear_inches
+    # tire goes around rim twice for diameter
+    ratio * wheel.diameter
+  end
+
+  # Embeding wheel here, adds the idea that should only exists in gear context
+  Wheel = Struct.new(:rim, :tire) do
+    def diameter
+      rim + (tire * 2)
+    end
   end
 end
 
@@ -16,3 +29,9 @@ end
 
 # puts Gear.new(30, 27).ratio
 # => 1.1111111111111112
+
+# puts Gear.new(52, 11, 26, 1.5).gear_inches
+# => 137.0909090909091
+
+# puts Gear.new(52, 11, 24, 1.25).gear_inches
+# =>125.27272727272728
