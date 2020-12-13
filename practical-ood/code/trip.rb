d8000a8d58aae74a4d89074a31d5beb5123195ba
janvmusic <jornunez@tripadvisor.com>
2020-12-13 16:47:38 -0700

    OOD: Trip now handles message prepare

--- practical-ood/code/trip.rb
+++ practical-ood/code/trip.rb
@@ -4,15 +4,7 @@ class Trip
   # The idea is not depend of what object is but what it does
   def prepare(preparers)
     preparers.each {|preparer|
-      case prepared
-      when Mechanic
-        preparer.prepare_bicycles(bicycles)
-      when TripCoordinator
-        preparer.buy_food(customers)
-      when Driver
-        preparer.gas_up(vehicle)
-        preparer.fill_water_tank(vechicle)
-      end
+      preparer.prepare_trip(self)
     }
   end
 
