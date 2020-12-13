c29a83de99b394341959945fe36ca34e6dde5201
janvmusic <jornunez@tripadvisor.com>
2020-12-13 16:31:45 -0700

    OOD: Expose prepare problem

--- practical-ood/code/trip.rb
+++ practical-ood/code/trip.rb
@@ -2,8 +2,18 @@ class Trip
   attr_reader :bicycles, :customers, :vehicle
 
   # The idea is not depend of what object is but what it does
-  def prepare(mechanic)
-    mechanic.prepare_bicycles(bicycles)
+  def prepare(preparers)
+    preparers.each {|preparer|
+      case prepared
+      when Mechanic
+        preparer.prepare_bicycles(bicycles)
+      when TripCoordinator
+        preparer.buy_food(customers)
+      when Driver
+        preparer.gas_up(vehicle)
+        preparer.fill_water_tank(vechicle)
+      end
+    }
   end
 
   # ...
