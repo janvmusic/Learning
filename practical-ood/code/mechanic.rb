72aca8a842873e68eccc13650669838896894358
janvmusic <jornunez@tripadvisor.com>
2020-12-13 16:46:11 -0700

    OOD: Duck typed preparers

--- practical-ood/code/mechanic.rb
+++ practical-ood/code/mechanic.rb
@@ -1,6 +1,6 @@
 class Mechanic
-  def prepare_bicycles(bicycles)
-    bicycles.each {|bicycle| prepare_bicycle}
+  def prepare_trip(trip)
+    trip.bicycles.each {|bicycle| prepare_bicycle(bicycle)}
   end
 
   def prepare_bicycle(bicycle)
