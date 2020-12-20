92cc7a91efb05a052f3321372e2feb0f09750aaf
janvmusic <jornunez@tripadvisor.com>
2020-12-20 16:31:24 -0700

    OOD: Modification to include more characteristics to bicycle

--- practical-ood/code/bicycle.rb
+++ practical-ood/code/bicycle.rb
@@ -1,25 +1,51 @@
 class Bicycle
+  attr_reader :style, :size, 
+              :tape_color,
+              :front_shock, :rear_shock
+  
   def initialize(**opts)
-    @size       = opts[:size]
-    @tape_color = opts[:tape_color]
+    @style       = opts[:style]
+    @size        = opts[:size]
+    @tape_color  = opts[:tape_color]
+    @front_shock = opts[:front_shock]
+    @rear_shock  = opts[:rear_shock]
   end
 
   # Every bike has the same defaults for tire and chain size
   def spares 
-    { 
-      chain:      '11-speed'
-      tire_size:  '23'
-      tape_color: tape_color
-    }
+    if style == :road
+      { 
+        chain:      '11-speed'
+        tire_size:  '23' # Millimeters
+        tape_color: tape_color
+      }
+    else 
+      {
+        chain:       '11-speed'
+        tire_size:   '2,1', # inches
+        front_shock: front_shock
+      }
+    end
   end
 
   # Many other methods
 end
 
 bike = Bicycle.new(
-  size:       'M'
+  style:      :road,
+  size:       'M',
   tape_color: 'red'
 )
 
 puts bike.size # M
 puts bike.spares # { :chain => "11-speed", tire_size => "23", tape_color => "red" }
+
+bike = Bicycle.new(
+  style:       :mountain
+  size:        'S'
+  front_shock: 'Manitou',
+  rear_shock:  'Fox'
+)
+
+puts bike.size # S
+puts bike.spares # { :chain => "11-speed", tire_size => "2.1", front_shock => "Manitou" }
