878876fa9e4389db89d3f02249aecfda020b79ed
janvmusic <1697540+janvmusic@users.noreply.github.com>
2023-11-29 22:23:00 -0600

    add last example

--- dynamic-programming/allConstruct.js
+++ dynamic-programming/allConstruct.js
@@ -45,16 +45,37 @@ const allConstructMemoized = (target, wordBank, memo = {}) => {
   return result;
 };
 
+const allConstructTabulated = (target, wordBank) => {
+  const table = Array(target.length + 1)
+    .fill()
+    .map(() => []);
+
+  // seeds
+  table[0] = [[]];
+
+  for (let i = 0; i < target.length; i++) {
+    for (const word of wordBank) {
+      const isSameWord = word === target.slice(i, i + word.length);
+      if (!isSameWord) {
+        continue;
+      }
+
+      const currentWays = table[i];
+      const newWays = currentWays.map((way) => [...way, word]);
+      table[i + word.length].push(...newWays);
+    }
+  }
+
+  return table[target.length];
+};
+
+console.log("Memoized ======");
 console.log(allConstructMemoized("", ["cat", "dog", "mouse"])); // [[]]
-
 console.log(allConstructMemoized("hello", ["cat", "dog", "mouse"])); // []
-
 console.log(allConstructMemoized("purple", ["purp", "p", "ur", "le", "purpl"])); // [[purp, le], [p, ur, p, le]]
-
 console.log(
   allConstructMemoized("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"])
 ); // [[ab, cd, ef], [ab, c, def], [abc, def], [abcd, ef]]
-
 console.log(
   allConstructMemoized("skateboard", [
     "bo",
@@ -66,14 +87,42 @@ console.log(
     "boar",
   ])
 ); // []
-
 console.log(
-  allConstructMemoized("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaz", [
+  allConstructMemoized("aaaaaaaaaaaaaaaaaaz", [
     "a",
     "aa",
     "aaa",
     "aaaa",
     "aaaaa",
   ])
-);
-[];
+); //[];
+
+console.log("Tabulated ======");
+console.log(allConstructTabulated("", ["cat", "dog", "mouse"])); // [[]]
+console.log(allConstructTabulated("hello", ["cat", "dog", "mouse"])); // []
+console.log(
+  allConstructTabulated("purple", ["purp", "p", "ur", "le", "purpl"])
+); // [[purp, le], [p, ur, p, le]]
+console.log(
+  allConstructTabulated("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"])
+); // [[ab, cd, ef], [ab, c, def], [abc, def], [abcd, ef]]
+console.log(
+  allConstructTabulated("skateboard", [
+    "bo",
+    "rd",
+    "ate",
+    "t",
+    "ska",
+    "sk",
+    "boar",
+  ])
+); // []
+console.log(
+  allConstructTabulated("aaaaaaaaaaaaaaaaaaz", [
+    "a",
+    "aa",
+    "aaa",
+    "aaaa",
+    "aaaaa",
+  ])
+); //[];
